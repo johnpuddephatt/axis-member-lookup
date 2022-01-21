d761f3284cd4ef4fe6686ccaea7ed8b9895d72fb
John Puddephatt <john@jdp.org.uk>
2022-01-21 18:47:43 +0000

    other api key

--- netlify/functions/member-lookup.js
+++ netlify/functions/member-lookup.js
@@ -6,18 +6,17 @@ exports.handler = async function () {
     api_key: process.env.CHARGEBEE_API_KEY,
   });
 
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: process.env.CHARGEBEE_API_KEY,
-    }),
-  };
-
   return chargebee.customer
     .list({
       "email[is]": "signmakers@email.com",
     })
     .request(function (error, result) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: process.env.CHARGEBEE_API_KEY,
+        }),
+      };
       // if (error) {
       //   return {
       //     statusCode: 404,
