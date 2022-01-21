987bbc23012d12b54ce4624ac8d368b0e5a629c5
John Puddephatt <john@jdp.org.uk>
2022-01-21 18:35:00 +0000

    hw2

--- netlify/functions/member-lookup.js
+++ netlify/functions/member-lookup.js
@@ -6,32 +6,31 @@ exports.handler = async function () {
     api_key: process.env.CHARGEBEE_API_KEY,
   });
 
-  return {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: "hello world",
-    }),
-  };
-
   return chargebee.customer
     .list({
       email: "signmakers@email.com",
     })
     .request(function (error, result) {
-      if (error) {
-        return {
-          statusCode: 404,
-          body: JSON.stringify({
-            message: error.message,
-          }),
-        };
-      } else {
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: result.data,
-          }),
-        };
-      }
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "hello world 2",
+        }),
+      };
+      // if (error) {
+      //   return {
+      //     statusCode: 404,
+      //     body: JSON.stringify({
+      //       message: error.message,
+      //     }),
+      //   };
+      // } else {
+      //   return {
+      //     statusCode: 200,
+      //     body: JSON.stringify({
+      //       message: result.data,
+      //     }),
+      //   };
+      // }
     });
 };
