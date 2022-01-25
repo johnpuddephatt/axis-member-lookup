81ecdc603cb4016df95eb344e00237e9b48944e7
John Puddephatt <john@jdp.org.uk>
2022-01-25 23:15:38 +0000

    improve promise structure

--- netlify/functions/member-lookup.js
+++ netlify/functions/member-lookup.js
@@ -1,37 +1,50 @@
 var chargebee = require("chargebee");
-const url = require("url");
-// const { customer } = require("chargebee/lib/resources/api_endpoints");
+
+const getCustomer = function (email) {
+  return chargebee.customer
+    .list({
+      "email[is]": email,
+    })
+    .request(function (error, result) {});
+};
+
+const getUserSubscriptionCount = function (user_id) {
+  return chargebee.subscription
+    .list({
+      "customer_id[is]": user_id,
+      "status[is]": "active",
+    })
+    .request(function (error, result) {});
+};
 
 exports.handler = async (event) => {
-  if (!event.queryStringParameters.email) {
-    return {
-      statusCode: 404,
-      body: "Query parameter 'email' was missing.",
-    };
-  }
+  let email = event.queryStringParameters.email;
+
   chargebee.configure({
     site: "axisweb-test",
-    // api_key: process.env.CHARGEBEE_API_KEY,
     api_key: "test_4S9iBXyvGhqA1Ib4cdDxg1z2cuKzxJjcu3m",
   });
 
-  return chargebee.customer
-    .list({
-      "email[is]": event.queryStringParameters.email,
-    })
-    .request(function (error, result) {})
+  return getCustomer(email)
     .then(function (result) {
-      return chargebee.subscription
-        .list({
-          "customer_id[is]": result.list[0].customer.id,
-          "status[is]": "active",
-        })
-        .request(function (error, result) {})
-        .then(function (result) {
-          return {
-            statusCode: 200,
-            body: JSON.stringify(!!result.list.length),
-          };
-        });
+      if (!email) {
+        throw new Error("No email!");
+      }
+      if (!result.list[0]) {
+        throw new Error("Customer not found!");
+      }
+      return getUserSubscriptionCount(result.list[0]?.customer.id);
+    })
+    .then(function (result) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(!!result.list.length),
+      };
+    })
+    .catch(function (error) {
+      return {
+        statusCode: 404,
+        body: "false",
+      };
     });
 };
